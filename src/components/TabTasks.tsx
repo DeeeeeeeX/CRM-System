c6b2a7f38899dd056fd04f42990c3906b5c7d1c0
DeeeeeeeX <parabala58@gmail.com>
2026-02-03 20:34:56 +0300

    fixed classname tab

--- src/components/TabTasks.tsx
+++ src/components/TabTasks.tsx
@@ -7,25 +7,22 @@ const TabTasks: React.FC<{
   setActiveTab: Dispatch<SetStateAction<string>>;
   activeTab: string;
 }> = ({ quantity = {}, setActiveTab, activeTab }) => {
-  let active = 'active';
-  let inActive = 'inActive';
-
   const { all = 0, completed = 0, inWork = 0 } = quantity;
 
   return (
     <nav>
-      <div onClick={() => setActiveTab('all')} className={activeTab === 'all' ? active : inActive}>
+      <div onClick={() => setActiveTab('all')} className={activeTab === 'all' ? 'active' : ''}>
         Всё ({all})
       </div>
       <div
         onClick={() => setActiveTab('inWork')}
-        className={activeTab === 'inWork' ? active : inActive}
+        className={activeTab === 'inWork' ? 'active' : ''}
       >
         В работе ({inWork})
       </div>
       <div
         onClick={() => setActiveTab('complete')}
-        className={activeTab === 'complete' ? active : inActive}
+        className={activeTab === 'complete' ? 'active' : ''}
       >
         Сделано ({completed})
       </div>
