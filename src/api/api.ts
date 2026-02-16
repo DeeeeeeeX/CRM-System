219d04b1fe1f4c47a313f8cc0e21c2a9921ab0e9
DeeeeeeeX <parabala58@gmail.com>
2026-02-16 17:48:01 +0300

    fix unused type

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
-import { ActiveTabs, MetaResponse, Todo, TodoInfo } from '../types/types';
+import {ActiveTabs, MetaResponse, Todo, TodoInfo} from '../types/types';
 
 export const baseUrl = 'https://easydev.club/api/v1/todos';
 
 export async function getToDos(activeTab: ActiveTabs): Promise<MetaResponse<Todo, TodoInfo>> {
   try {
-    let response = await fetch(baseUrl + `?filter=${activeTab}`, { method: 'GET' });
+    let response = await fetch(baseUrl + `?filter=${activeTab}`, {method: 'GET'});
     return await response.json();
   } catch (error) {
     throw error;
@@ -15,24 +15,18 @@ export async function deleteToDo(id: number): Promise<void> {
   try {
     await fetch(`${baseUrl}/${id}`, {
       method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
     });
   } catch (error) {
     throw error;
   }
 }
 
-export async function editToDo(
-  id: number,
-  taskState: {
-    title?: string;
-    isDone?: boolean;
-  },
-): Promise<void> {
+export async function editToDo(id: number, taskState: Todo): Promise<void> {
   try {
     await fetch(`${baseUrl}/${id}`, {
       method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(taskState),
     });
   } catch (error) {
@@ -44,8 +38,8 @@ export async function createToDo(title: string): Promise<void> {
   try {
     await fetch(baseUrl, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ isDone: false, title }),
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({isDone: false, title}),
     });
   } catch (error) {
     throw error;
